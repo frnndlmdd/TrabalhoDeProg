b5ced61f18d490331257661d2e99c6852a4c290e
Fernando Almeida <fernandoalmeida1772@gmail.com>
2022-03-23 17:51:08 -0300

    Adicionar arquivos de projeto.

---
+++ Calendario.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoDeProg
+{
+    public class Calendario
+    {
+        private UInt16 ano {get;set;}
+
+        private Byte mes {get;set;}
+
+
+
+        public Calendario(UInt16 _ano, Byte _mes)
+        {
+            _ano = _ano;
+            _mes = _mes;
+        }
+
+        public Boolean IsMesValido()
+        {
+            if (mes > 0 && mes < 13)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        public Boolean IsAnoValido()
+        {
+            if (ano > 0 && mes < 1000)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        public String GerarCalendario()
+        {
+            return "Não sei fazer issokkk";
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabalhoDeProg
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+           
+            String me;
+            Byte _me;
+            String an;
+            UInt16 v = 1;
+
+
+            do
+            {
+                Console.WriteLine("Digite o MÊS desejado: ");
+                me = Console.ReadLine();
+                _me = Convert.ToByte(me);
+
+                if (_me > 0 && _me < 13)
+                {
+                    v = 3;
+                }
+                else
+                {
+                    Console.WriteLine("-----------------------------------------");
+                    Console.WriteLine("O MÊS deve seguir o formato numérico,\ntal como '10' para Outubro,\ne, portanto, deve ser de 1 a 12.");
+                    Console.WriteLine("-----------------------------------------");
+                }
+            }
+            while (v < 2);
+
+            
+
+            
+
+
+
+
+            //Console.WriteLine("\n\nDigite o ANO desejado: ");
+            //an = Console.ReadLine();
+
+
+            //Calendario cal = new Calendario()
+
+
+        }
+    }
+}
